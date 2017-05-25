e2738b5a06326445b9f4407843f8f8f724548bad
Alessandro Francesco De Rosis <alessandrof.derosis@gmail.com>
2017-05-25 10:01:31 +0200

    numerical integration ok

--- MPI/NumericalIntegration/main.c
+++ MPI/NumericalIntegration/main.c
@@ -2,14 +2,15 @@
 
 #include "mpi.h"
 
-float calculateIntegral(float& a, float& b, int& n, float& h);
+float calculateIntegral(float a, float b, int n, float h);
+double get_max_time(double partialTime, int my_rank, int p);
 
 int main (int argc, char** argv)
 {
     int my_rank, numprocs;
 
-    float a=0.0, b=1.0;
-    int n= 1024; //numeri di trapezoidi
+    float a=0.0, b=9.0;
+    int n= 999999999; //numeri di trapezoidi
     float h; //base singoli trapeziodi
     float local_a, local_b, local_n;
 
@@ -18,6 +19,7 @@ int main (int argc, char** argv)
     int source;
     int dest=0;
     int tag=0;
+    double start = 0, finish = 0, partialTime = 0;
 
     MPI_Status status;
 
@@ -27,6 +29,9 @@ int main (int argc, char** argv)
     MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
 
 
+    MPI_Barrier(MPI_COMM_WORLD);
+    start = MPI_Wtime();
+
     h= (b-a)/n;
     local_n = n/numprocs;
 
@@ -35,12 +40,54 @@ int main (int argc, char** argv)
 
     integral =calculateIntegral(local_a, local_b, local_n, h);
 
+    if(my_rank == 0)
+    {
+      total = integral;
+      for(source = 1; source < numprocs ;source++)
+      {
+        MPI_Recv(&integral,1,MPI_FLOAT,source,tag,MPI_COMM_WORLD,&status);
+        total += integral;
+      }
+    }
+    else
+    {
+      MPI_Send(&integral,1,MPI_FLOAT,dest,tag,MPI_COMM_WORLD);
+    }
+
+    finish = MPI_Wtime();
+
+    partialTime = finish - start;
+
+    partialTime = get_max_time(partialTime, my_rank, numprocs);
+
+    if(my_rank == 0)
+    {
+      printf("With n = %d number of trapezioids, our estimate of integral from %f to %f = %f\n",n,a,b,total);
+      printf("Time = %f\n",partialTime );
+    }
 
 
 
+    MPI_Finalize();
 
 
 }
+
+double get_max_time(double partialTime, int my_rank, int p) {
+   MPI_Status status;
+   double temp;
+
+   if (my_rank == 0) {
+      for (int source = 1; source < p; source++) {
+         MPI_Recv(&temp, 1, MPI_DOUBLE, source, 0, MPI_COMM_WORLD, &status);
+         if (temp > partialTime) partialTime = temp;
+      }
+   } else {
+      MPI_Send(&partialTime, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+   }
+   return partialTime;
+}
+
 float f(float x)
 {
     return x*x;
@@ -48,7 +95,7 @@ float f(float x)
 
 }
 
-float calculateIntegral(float& a, float& b, int& n, float& h)
+float calculateIntegral(float a, float b, int n, float h)
 {
 
     float integral =(f(a) + f(b))/2.0;;
