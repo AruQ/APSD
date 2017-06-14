e0ecddef537dcb6ac85e7a71ce1edda40cc52fc0
AruQ <arcuri.paola.93@gmail.com>
2017-06-14 20:27:54 +0200

    fixed matrix implementation

--- MPI/Matrix Multiplication/TipoB/main.c
+++ MPI/Matrix Multiplication/TipoB/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <math.h>
+#include <cmath>
 
 
 MPI_Status status;
@@ -15,7 +15,7 @@ inline int get (int i, int j, int nCol)
 int numnodes,myid,mpi_err;
 #define mpi_root 0
 
-#define DIMENSION 8
+const int DIMENSION= 16*100;
 
 
 void init_it(int  *argc, char ***argv);
@@ -31,7 +31,7 @@ int main(int argc,char *argv[]){
     int * local_c;
     int * local_b;
     int * b, *a, *c;
-    int* counts;
+
     double start = 0, finish = 0, partialTime = 0;
 
     init_it(&argc,&argv);
@@ -47,36 +47,11 @@ int main(int argc,char *argv[]){
 
     b = (int*)malloc(sizeof(int)*DIMENSION * DIMENSION);
 
-    //    int* displs =(int*) malloc( numnodes * sizeof(int) );
 
-
-
-
-
-    //    counts=(int*)malloc(sizeof(int)*numnodes);
-
-    //    for (int i = 0; i < numnodes; ++i) {
-    //        counts[i] = DIMENSION/(numnodes);
-    //        if (i == numnodes-1)
-    //            counts[i] += DIMENSION%numnodes;
-
-    //        counts[i] *=DIMENSION;
-
-
-    //    }
-
-    //    displs[0]=0;
-    //    for(int i=1;i<numnodes;i++){
-    //        displs[i]=counts[i-1]+displs[i-1];
-    //    }
-
-
-
-    //    printf("sono count di myid  %d count: %d e il mio displ è %d\n", myid,counts[myid], displs[myid]);
 
 
     int nColumns = DIMENSION / sqrt(numnodes-1);
-    int stride = DIMENSION-nColumns;
+    int stride = DIMENSION;
     if(myid == mpi_root){
 
         a = (int*)malloc(sizeof(int)*DIMENSION * DIMENSION);
@@ -88,24 +63,18 @@ int main(int argc,char *argv[]){
         }
         c = (int*)malloc(sizeof(int)*DIMENSION * DIMENSION);
 
-        printf("sono qui\n");
+
 
         MPI_Type_vector(DIMENSION, nColumns, stride,MPI_INT,&columnsType);
         MPI_Type_commit(&columnsType);
 
         for (int dest =1; dest<numnodes; dest++ )
         {
-
-            int starterIndexB = (dest-1)*nColumns %DIMENSION;
-            int starterIndexA = (dest-1)*nColumns *DIMENSION;
-            printf ("starter index %d nCol %d stride %d \n", starterIndexB, nColumns, stride);
-            //            MPI_Send (&a[starterIndexA], nColumns*DIMENSION, MPI_INT, dest, 0, MPI_COMM_WORLD);
-            MPI_Send (&b[starterIndexB], nColumns, columnsType, dest, 0, MPI_COMM_WORLD);
+            int starterIndexB = ((dest-1)*nColumns) %DIMENSION;
+            int starterIndexA = (dest-1)*nColumns *DIMENSION % (DIMENSION*DIMENSION);
+            MPI_Send (&a[starterIndexA], nColumns*DIMENSION, MPI_INT, dest, 2, MPI_COMM_WORLD);
+            MPI_Send (&b[starterIndexB], 1, columnsType, dest, 0, MPI_COMM_WORLD);
         }
-        //        for (int dest = 1; dest<numnodes; ++dest) {
-        //            MPI_Send(b, DIMENSION*DIMENSION, MPI_INT, dest, mpi_root, MPI_COMM_WORLD);
-
-        //        }
 
     }
 
@@ -115,29 +84,25 @@ int main(int argc,char *argv[]){
     local_c = (int*)malloc(sizeof(int)*nColumns* nColumns);
 
 
-
-
-    printf ("sizeof(int)*nColumns* DIMENSION %d \n", sizeof(int)*nColumns* DIMENSION);
-
     if (myid != mpi_root)
     {
 
-        printf ("sono qui %d\n", nColumns * DIMENSION);
-//        MPI_Recv(local_a, nColumns*DIMENSION, MPI_INT, mpi_root, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(local_b, DIMENSION, MPI_INT, mpi_root, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(local_a, nColumns*DIMENSION, MPI_INT, mpi_root, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(local_b, DIMENSION*nColumns, MPI_INT, mpi_root, 0, MPI_COMM_WORLD, &status);
 
-        //        for (int i = 0; i < nColumns; ++i) {
-        //            for (int j = 0; j < nColumns; ++j) {
-        //                for (int k = 0; k < DIMENSION; ++k) {
-        //                    local_c[get(i,j,DIMENSION)] += local_a[get(i,k,DIMENSION)] * b[get(i,k,DIMENSION)];
+        for (int i = 0; i < nColumns; ++i) {
+            for (int j = 0; j < nColumns; ++j) {
 
-        //                }
-        //            }
+                local_c[get(i,j,nColumns)]=0;
+                for (int k = 0; k < DIMENSION; ++k) {
+                    local_c[get(i,j,nColumns)] += local_a[get(i,k,DIMENSION)] * local_b[get(j,k,DIMENSION)];
+                }
 
-        MPI_Type_vector(nColumns, nColumns, stride, MPI_INT, &subMatrixType);
-        MPI_Type_commit(&subMatrixType);
 
-        MPI_Send (local_c, nColumns, subMatrixType, mpi_root, 1, MPI_COMM_WORLD);
+            }
+        }
+
+        MPI_Send (local_c, nColumns*nColumns,MPI_INT, mpi_root, 1, MPI_COMM_WORLD);
 
 
 
@@ -146,11 +111,13 @@ int main(int argc,char *argv[]){
 
     if (myid == mpi_root)
     {
+        MPI_Type_vector(nColumns, nColumns, stride, MPI_INT, &subMatrixType);
+        MPI_Type_commit(&subMatrixType);
         int starterIndex = 0;
         for (int source = 1; source < numnodes; ++source) {
 
 
-            MPI_Recv(&c[starterIndex], DIMENSION, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&c[starterIndex], 1, subMatrixType, source, 1, MPI_COMM_WORLD, &status);
 
             if ((starterIndex+nColumns) % DIMENSION == 0)
             {
@@ -161,55 +128,35 @@ int main(int argc,char *argv[]){
         }
     }
 
-    //    MPI_Scatterv(a,counts,displs, MPI_INT,local_a,counts[myid],MPI_INT, mpi_root,MPI_COMM_WORLD);
 
-    //    for (int i = 0; i < counts[myid]; ++i) {
+//    if (myid == mpi_root)
+//    {
+//        for (int i = 0; i< DIMENSION*DIMENSION; i++)
+//        {
+//            printf ("%d ", c[i]);
+//            if ((i+1)%DIMENSION ==0)
+//                printf ("\n");
+//        }
+//    }
 
-    //        local_c[i] = 0;
-    //    }
+        MPI_Barrier(MPI_COMM_WORLD);
+        finish = MPI_Wtime();
+        partialTime = finish - start;
 
-    //    for (int i = 0; i < counts[myid]/DIMENSION; ++i) {
-    //        for (int j = 0; j < DIMENSION; ++j) {
-    //            for (int k = 0; k < DIMENSION; ++k) {
-    //                local_c[get(i,j,DIMENSION)] += local_a[get(i,k,DIMENSION)] * b[get(k,j,DIMENSION)];
-
-    //            }
-    //        }
-
-    //    }
-
-    //    mpi_err = MPI_Gatherv(local_c,counts[myid], MPI_INT, c,counts ,displs, MPI_INT, mpi_root,MPI_COMM_WORLD);
-
-    if (myid == mpi_root)
-    {
-        //        for (int i = 0; i< DIMENSION*DIMENSION; i++)
-        //        {
-        //            printf ("%d ", c[i]);
-        //            if ((i+1)%DIMENSION ==0)
-        //                printf ("\n");
-        //        }
-    }
-
-    //    MPI_Barrier(MPI_COMM_WORLD);
-    //    finish = MPI_Wtime();
-    //    partialTime = finish - start;
-
-    //    if(myid == mpi_root)
-    //    {
-    //      printf("Time = %f\n",partialTime );
-    //    }
+        if(myid == mpi_root)
+        {
+          printf("Time = %f\n",partialTime );
+        }
 
     mpi_err = MPI_Finalize();
 
     if (myid == mpi_root)
     {
-        delete [] a;
-        delete [] b;
-        delete [] c;
+        free(a);
+        free(b);
+        free(c);
     }
-    //    delete [] local_a;
-    //    delete [] local_c;
-    //    delete [] local_b;
-    //    delete [] counts;
-    //    delete [] displs;
+    free(local_a);
+    free(local_b);
+    free(local_c);
 }
